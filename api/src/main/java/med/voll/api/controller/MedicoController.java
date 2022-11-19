1970e7eb6fb913043ce42ca93cf16c4e4563c923
Matheus Souza <matheus-oliveira17@hotmail.com>
2022-11-18 23:26:25 -0300

    Add cache para entidades médico e paciente

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -18,17 +20,20 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeMedicosAtivos", allEntries = true)
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
 
     @GetMapping
+    @Cacheable(value = "listaDeMedicosAtivos")
     public Page<DadosListagemMedico> listar(@PageableDefault(sort = {"nome"}) Pageable paginacao){
         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
     @Transactional
+    @CacheEvict(value = "listaDeMedicosAtivos", allEntries = true)
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInfo(dados);
@@ -36,6 +41,7 @@ public class MedicoController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeMedicosAtivos", allEntries = true)
     public void excluir(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.setAtivo(false);
--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -18,17 +20,20 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "cacheListagemPacientes", allEntries = true)
     public void cadastrar(@Valid @RequestBody DadosCadastroPaciente dados){ // DTO paciente
         pacienteRepository.save(new Paciente(dados));
     }
 
     @GetMapping
+    @Cacheable(value = "cacheListagemPacientes")
     public Page<DadosListagemPaciente> listagemPacientes(@PageableDefault(sort = {"nome"}) Pageable paginacao){
         return pacienteRepository.findAllByAtivo(paginacao, true).map(DadosListagemPaciente::new);
     }
 
     @PutMapping
     @Transactional
+    @CacheEvict(value = "cacheListagemPacientes", allEntries = true)
     public void atualizar(@Valid @RequestBody DadosAtualizacaoPaciente dados){
         var paciente = pacienteRepository.getReferenceById(dados.id());
         paciente.atualizarInfo(dados);
@@ -36,6 +41,7 @@ public class PacienteController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "cacheListagemPacientes", allEntries = true)
     public void excluir(@PathVariable Long id){
         var paciente = pacienteRepository.getReferenceById(id);
         paciente.setAtivo(false);
